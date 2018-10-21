f8a3badcdedbdc24148cbcf04904be5f0b24b283
max197a <max197aa@gmail.com>
2018-10-21 18:20:14 +0300

    add part

--- src/js/main.js
+++ src/js/main.js
@@ -28,6 +28,7 @@ $(document).ready(function() {
     initValidations();
     initScrollMonitor();
     initTeleport();
+    initTimer();
   }
 
   _window.on("resize", debounce(setBreakpoint, 200));
@@ -122,6 +123,44 @@ $(document).ready(function() {
       scrollTo: targetScroll,
       ease: easingSwing
     });
+  }
+
+  ////////////
+  // TIMER PLUGIN
+  ////////////
+  function initTimer(printable) {
+    if ($(".timer").length > 0) {
+      var countDownDate = new Date("November 16, 2018 09:00:00").getTime();
+
+      // Update the count down every 1 second
+      var x = setInterval(function() {
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("#days").innerHTML = "EXPIRED";
+        }
+      }, 1000);
+    }
   }
 
   ////////////
@@ -258,7 +297,7 @@ $(document).ready(function() {
         // when window width is <= 768px
         768: {
           slidesPerView: 3,
-          spaceBetween: 55
+          spaceBetween: 20
         },
         // when window width is <= 1268px
         1268: {
